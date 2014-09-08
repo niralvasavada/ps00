6f17abf003a184fa6adf1080815b01b8c864b008
niralvasavada <vasavada.n@husky.neu.edu>
2014-09-08 07:32:57 +0530

    Initial Commit

---
+++ 9.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |9|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+ (define (function a)
+    (= (modulo a 2) 0))
+       
+modulo is sign of divisor and remainder is sign of dividend
+    
